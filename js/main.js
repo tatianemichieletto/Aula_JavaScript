cd83b5ffd874ec2ec9165f57a336e289821440fd
Tatiane Michieletto <tatianemoraes12.tm@gmail.com>
2020-12-16 23:40:59 +0100

    Finalizado parte3 Condicionais, Laços de repetição e Date

--- js/main.js
+++ js/main.js
@@ -1,8 +1,38 @@
 
 
+/*
+var d = new Date();
+alert(d.getMonth()+1);
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes());
+*/
 
+/*
+var count;
+for (count=0; count <= 5; count++) {
+    alert(count);
+};
+*/
 
+/*
+var count = 0;
+while (count <= 5) {
+    console.log(count);
+    alert(count);
+    count++;
+}
+*/
 
+/*
+var idade = prompt("Qual é a sua idade?");
+var idade = 18;
+if (idade >= 18) {
+    alert("maior de idade");
+} else{
+    alert("menor de idade");
+};
+*/
 
 /*
 var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
