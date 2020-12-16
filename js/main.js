dfeb1c947b94bf7afe5b4042f52ef62482cb5b01
Tatiane Michieletto <tatianemoraes12.tm@gmail.com>
2020-12-16 23:17:59 +0100

    Finalizado parte2 Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
 
+
+
+
+
+
+/*
+var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+/*
+var fruta = {nome: "maçã", cor: "vermelha"}
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+//var lista = ["maçã" , "pera" , "laranja"];
+//lista.push("uva");
+//lista.pop();
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" - "));
+//alert(lista[1]);
+
 //var nome = "Tatiane Michieletto";
 //var n1 = 5;
 //var n2 = 3;
