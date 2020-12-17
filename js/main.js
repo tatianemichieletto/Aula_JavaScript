51ea522691f8d58f711c074e0cdce3861e927dcc
Tatiane Michieletto <tatianemoraes12.tm@gmail.com>
2020-12-17 22:59:38 +0100

    Criando variáveis Local e Global

--- js/main.js
+++ js/main.js
@@ -1,5 +1,27 @@
 
 
+
+/*
+function soma(n1, n2) {
+    return n1 + n2;
+}
+
+var validar = 0;
+
+function validaIdade(idade) {
+    if(idade >=18) {
+        validar = true
+    } else{
+        validar = false
+    }
+    return validar;
+}
+
+var idade = prompt("Qual sua idade?");
+validaIdade(idade)
+console.log(validar);
+*/
+
 /*
 var d = new Date();
 alert(d.getMonth()+1);
